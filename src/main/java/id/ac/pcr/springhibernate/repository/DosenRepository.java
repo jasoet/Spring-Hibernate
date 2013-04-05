3536c85cc2092112369cc187fa240af4638b6d85
Deny Prasetyo <jasoet87@gmail.com>
2013-04-05 08:25:17 +0700

    Fix Pom.xml

--- src/main/java/id/ac/pcr/springhibernate/repository/DosenRepository.java
+++ src/main/java/id/ac/pcr/springhibernate/repository/DosenRepository.java
@@ -1,7 +1,11 @@
 package id.ac.pcr.springhibernate.repository;
 
 import id.ac.pcr.springhibernate.model.Dosen;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,4 +15,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * To change this template use File | Settings | File Templates.
  */
 public interface DosenRepository extends PagingAndSortingRepository<Dosen, Long> {
+
+    @Query("SELECT o FROM Dosen o WHERE o.niy=:niy AND o.nama=:asdf")
+    List<Dosen> findByNiyAndNama(@Param("niy") String niy, @Param("asdf") String nama);
 }
